cdf0964660feb66e99d637a18570cea99915bb80
Hongpeng Guo <hg5@illinois.edu>
2019-10-20 15:44:29 -0500

    tune hyper 1a

--- MPs/MP5/1a/BOW_sentiment_analysis.py
+++ MPs/MP5/1a/BOW_sentiment_analysis.py
@@ -13,146 +13,158 @@ import io
 
 from BOW_model import BOW_model
 
-#imdb_dictionary = np.load('../preprocessed_data/imdb_dictionary.npy')
-vocab_size = 8000
 
-x_train = []
-with io.open('../preprocessed_data/imdb_train.txt','r',encoding='utf-8') as f:
-    lines = f.readlines()
-for line in lines:
-    line = line.strip()
-    line = line.split(' ')
-    line = np.asarray(line,dtype=np.int)
+def main(input_optimizer, input_batch_size, input_hidden_units, input_epochs):
+	#imdb_dictionary = np.load('../preprocessed_data/imdb_dictionary.npy')
+	vocab_size = 8000
 
-    line[line>vocab_size] = 0
+	x_train = []
+	with io.open('../preprocessed_data/imdb_train.txt','r',encoding='utf-8') as f:
+		lines = f.readlines()
+	for line in lines:
+		line = line.strip()
+		line = line.split(' ')
+		line = np.asarray(line,dtype=np.int)
 
-    x_train.append(line)
-x_train = x_train[0:25000]
-y_train = np.zeros((25000,))
-y_train[0:12500] = 1
+		line[line>vocab_size] = 0
 
-x_test = []
-with io.open('../preprocessed_data/imdb_test.txt','r',encoding='utf-8') as f:
-    lines = f.readlines()
-for line in lines:
-    line = line.strip()
-    line = line.split(' ')
-    line = np.asarray(line,dtype=np.int)
+		x_train.append(line)
+	x_train = x_train[0:25000]
+	y_train = np.zeros((25000,))
+	y_train[0:12500] = 1
 
-    line[line>vocab_size] = 0
+	x_test = []
+	with io.open('../preprocessed_data/imdb_test.txt','r',encoding='utf-8') as f:
+		lines = f.readlines()
+	for line in lines:
+		line = line.strip()
+		line = line.split(' ')
+		line = np.asarray(line,dtype=np.int)
 
-    x_test.append(line)
-y_test = np.zeros((25000,))
-y_test[0:12500] = 1
+		line[line>vocab_size] = 0
 
-vocab_size += 1
-no_hidden_units = 500
+		x_test.append(line)
+	y_test = np.zeros((25000,))
+	y_test[0:12500] = 1
 
-model = BOW_model(vocab_size, no_hidden_units)
-model.cuda()
+	vocab_size += 1
+	# no_hidden_units = 500
+	no_hidden_units = input_hidden_units
 
-# opt = 'sgd'
-# LR = 0.01
-opt = 'adam'
-LR = 0.001
-if(opt=='adam'):
-    optimizer = optim.Adam(model.parameters(), lr=LR)
-elif(opt=='sgd'):
-    optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
+	model = BOW_model(vocab_size, no_hidden_units)
+	model.cuda()
 
-batch_size = 200
-no_of_epochs = 6
-L_Y_train = len(y_train)
-L_Y_test = len(y_test)
+	# opt = 'sgd'
+	# LR = 0.01
+	# opt = 'adam'
+	opt = input_optimizer
+	LR = 0.001
+	if(opt=='adam'):
+		optimizer = optim.Adam(model.parameters(), lr=LR)
+	elif(opt=='sgd'):
+		optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
 
-model.train()
+	# batch_size = 200
+	batch_size = input_batch_size
+	# no_of_epochs = 6
+	no_of_epochs = input_epochs
+	L_Y_train = len(y_train)
+	L_Y_test = len(y_test)
 
-train_loss = []
-train_accu = []
-test_accu = []
+	model.train()
 
-print ("Optmizer: %s" % opt, "LR: %.6f" % LR, "EpochSize: %d" % no_of_epochs, "BatchSize: %d" % batch_size, "VocalSize: %d" % (vocab_size-1), "HidenSize: %d" % no_hidden_units)
+	train_loss = []
+	train_accu = []
+	test_accu = []
 
-for epoch in range(no_of_epochs):
+	print ("Optmizer: %s" % opt, "LR: %.6f" % LR, "EpochSize: %d" % no_of_epochs, "BatchSize: %d" % batch_size, "VocalSize: %d" % (vocab_size-1), "HidenSize: %d" % no_hidden_units)
 
-    # training
-    model.train()
+	for epoch in range(no_of_epochs):
 
-    epoch_acc = 0.0
-    epoch_loss = 0.0
+		# training
+		model.train()
 
-    epoch_counter = 0
+		epoch_acc = 0.0
+		epoch_loss = 0.0
 
-    time1 = time.time()
-    
-    I_permutation = np.random.permutation(L_Y_train)
+		epoch_counter = 0
 
-    for i in range(0, L_Y_train, batch_size):
+		time1 = time.time()
+		
+		I_permutation = np.random.permutation(L_Y_train)
 
-        x_input = [x_train[j] for j in I_permutation[i:i+batch_size]]
-        y_input = np.asarray([y_train[j] for j in I_permutation[i:i+batch_size]],dtype=np.int)
-        target = Variable(torch.FloatTensor(y_input)).cuda()
+		for i in range(0, L_Y_train, batch_size):
 
-        optimizer.zero_grad()
-        loss, pred = model(x_input,target)
-        loss.backward()
+			x_input = [x_train[j] for j in I_permutation[i:i+batch_size]]
+			y_input = np.asarray([y_train[j] for j in I_permutation[i:i+batch_size]],dtype=np.int)
+			target = Variable(torch.FloatTensor(y_input)).cuda()
 
-        optimizer.step()   # update weights
-        
-        prediction = pred >= 0.0
-        truth = target >= 0.5
-        acc = prediction.eq(truth).sum().cpu().data.numpy()
-        epoch_acc += acc
-        epoch_loss += loss.data.item()
-        epoch_counter += batch_size
+			optimizer.zero_grad()
+			loss, pred = model(x_input,target)
+			loss.backward()
 
-    epoch_acc /= epoch_counter
-    epoch_loss /= (epoch_counter/batch_size)
+			optimizer.step()   # update weights
+			
+			prediction = pred >= 0.0
+			truth = target >= 0.5
+			acc = prediction.eq(truth).sum().cpu().data.numpy()
+			epoch_acc += acc
+			epoch_loss += loss.data.item()
+			epoch_counter += batch_size
 
-    train_loss.append(epoch_loss)
-    train_accu.append(epoch_acc)
+		epoch_acc /= epoch_counter
+		epoch_loss /= (epoch_counter/batch_size)
 
-    print(epoch, "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss, "%.4f" % float(time.time()-time1))
+		train_loss.append(epoch_loss)
+		train_accu.append(epoch_acc)
 
-    # ## test
-    model.eval()
+		print(epoch, "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss, "%.4f" % float(time.time()-time1))
 
-    epoch_acc = 0.0
-    epoch_loss = 0.0
+		# ## test
+		model.eval()
 
-    epoch_counter = 0
+		epoch_acc = 0.0
+		epoch_loss = 0.0
 
-    time1 = time.time()
-    
-    I_permutation = np.random.permutation(L_Y_test)
+		epoch_counter = 0
 
-    for i in range(0, L_Y_test, batch_size):
+		time1 = time.time()
+		
+		I_permutation = np.random.permutation(L_Y_test)
 
-        x_input = [x_test[j] for j in I_permutation[i:i+batch_size]]
-        y_input = np.asarray([y_test[j] for j in I_permutation[i:i+batch_size]],dtype=np.int)
-        target = Variable(torch.FloatTensor(y_input)).cuda()
+		for i in range(0, L_Y_test, batch_size):
 
-        with torch.no_grad():
-            loss, pred = model(x_input,target)
-        
-        prediction = pred >= 0.0
-        truth = target >= 0.5
-        acc = prediction.eq(truth).sum().cpu().data.numpy()
-        epoch_acc += acc
-        epoch_loss += loss.data.item()
-        epoch_counter += batch_size
+			x_input = [x_test[j] for j in I_permutation[i:i+batch_size]]
+			y_input = np.asarray([y_test[j] for j in I_permutation[i:i+batch_size]],dtype=np.int)
+			target = Variable(torch.FloatTensor(y_input)).cuda()
 
-    epoch_acc /= epoch_counter
-    epoch_loss /= (epoch_counter/batch_size)
+			with torch.no_grad():
+				loss, pred = model(x_input,target)
+			
+			prediction = pred >= 0.0
+			truth = target >= 0.5
+			acc = prediction.eq(truth).sum().cpu().data.numpy()
+			epoch_acc += acc
+			epoch_loss += loss.data.item()
+			epoch_counter += batch_size
 
-    test_accu.append(epoch_acc)
+		epoch_acc /= epoch_counter
+		epoch_loss /= (epoch_counter/batch_size)
 
-    time2 = time.time()
-    time_elapsed = time2 - time1
+		test_accu.append(epoch_acc)
 
-    print("  ", "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss)
+		time2 = time.time()
+		time_elapsed = time2 - time1
 
-torch.save(model,'BOW.model')
-data = [train_loss,train_accu,test_accu]
-data = np.asarray(data)
-np.save('data.npy',data)
+		print("  ", "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss)
+
+	torch.save(model,'BOW.model')
+	data = [train_loss,train_accu,test_accu]
+	data = np.asarray(data)
+	np.save('data.npy',data)
+
+if __name__ == "__main__":
+	main(input_optimizer='adam', input_batch_size='200', input_hidden_units='500', input_epochs='6')
+	main(input_optimizer='adam', input_batch_size='200', input_hidden_units='300', input_epochs='6')
+	main(input_optimizer='adam', input_batch_size='200', input_hidden_units='800', input_epochs='10')
+	main(input_optimizer='adam', input_batch_size='500', input_hidden_units='800', input_epochs='10')
